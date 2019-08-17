81962213c97d436824e34ddd2752fa1d69e7c5ad
DreaMer963 <wxd963996380@gmail.com>
2019-08-17 10:29:47 +0800

    update

--- simple-db/turnInLab6.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git tag -d lab6submit
-git push origin :refs/tags/lab6submit
-git add --all .
-git commit -a -m 'Lab 6'
-git tag -a lab6submit -m 'submit lab 6'
-git push origin master --tags
