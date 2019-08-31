15bc82f4dbb2b61bfb6818899fb5a05a7949b0d0
DreaMer963 <wxd963996380@gmail.com>
2019-08-31 17:16:59 +0800

    finish Predicate.java

--- simple-db/src/simpledb/Predicate.java
+++ simple-db/src/simpledb/Predicate.java
@@ -54,8 +54,14 @@ public class Predicate implements Serializable {
      * @param operand
      *            field value to compare passed in tuples to
      */
+    private int field;
+    private Op op;
+    private Field operand;
     public Predicate(int field, Op op, Field operand) {
         // some code goes here
+        this.field = field;
+        this.op = op;
+        this.operand = operand;
     }
 
     /**
@@ -64,7 +70,7 @@ public class Predicate implements Serializable {
     public int getField()
     {
         // some code goes here
-        return -1;
+        return field;
     }
 
     /**
@@ -73,7 +79,7 @@ public class Predicate implements Serializable {
     public Op getOp()
     {
         // some code goes here
-        return null;
+        return op;
     }
     
     /**
@@ -82,7 +88,7 @@ public class Predicate implements Serializable {
     public Field getOperand()
     {
         // some code goes here
-        return null;
+        return operand;
     }
     
     /**
@@ -97,7 +103,8 @@ public class Predicate implements Serializable {
      */
     public boolean filter(Tuple t) {
         // some code goes here
-        return false;
+        return t.getField(field).compare(op, operand);
+        //return operand.compare(op, t.getField(field));
     }
 
     /**
@@ -106,6 +113,6 @@ public class Predicate implements Serializable {
      */
     public String toString() {
         // some code goes here
-        return "";
+        return "f = "+field+" "+"op = "+op+" "+"operand = "+operand;
     }
 }
